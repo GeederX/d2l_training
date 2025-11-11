49f1481214744e322e670a536e0443b2709efac1
GeederX <Geeder@foxmail.com>
2025-11-11 17:30:38 +0800

    202511_LeNet

--- myrepo.py
+++ myrepo.py
@@ -4,6 +4,9 @@ import os
 from torch import nn
 from torch.utils import data
 from torchvision import transforms
+from matplotlib_inline import backend_inline
+from d2l import torch as d2l
+from IPython import display
 
 
 def load_data_fashion_mnist(batch_size, resize=None, num_threads=0):
@@ -37,17 +40,17 @@ class Accumulator:
         return self.data[idx]
 
 
-def accuracy(y, y_hat):
+def accuracy(y_hat, y):
     y_hat = y_hat.argmax(axis=-1)
     cmp = y_hat.type(y.dtype) == y
-    return float((cmp.type(y.dtype).sum()) / len(y))
+    return float(cmp.type(y.dtype).sum())
 
 
 def evaluate_accuracy(net, data_iter):
     metric = Accumulator(2)
     with torch.no_grad():
         for X, y in data_iter:
-            metric.add(accuracy(y, net(X)), 1)
+            metric.add(accuracy(net(X), y), y.numel())
     return metric[0] / metric[1]
 
 
@@ -82,3 +85,136 @@ def train(net, train_iter, test_iter, loss, updater, num_epochs):
 def init_weights(m):
     if type(m) == nn.Linear:
         nn.init.normal_(m.weight, std=0.01)
+
+
+class Animator:  # @save
+    """在动画中绘制数据"""
+
+    def __init__(
+        self,
+        xlabel=None,
+        ylabel=None,
+        legend=None,
+        xlim=None,
+        ylim=None,
+        xscale="linear",
+        yscale="linear",
+        fmts=("-", "m--", "g-.", "r:"),
+        nrows=1,
+        ncols=1,
+        figsize=(3.5, 2.5),
+    ):
+        # 增量地绘制多条线
+        if legend is None:
+            legend = []
+        d2l.use_svg_display()
+        self.fig, self.axes = d2l.plt.subplots(nrows, ncols, figsize=figsize)
+        if nrows * ncols == 1:
+            self.axes = [
+                self.axes,
+            ]
+        # 使用lambda函数捕获参数
+        self.config_axes = lambda: d2l.set_axes(
+            self.axes[0], xlabel, ylabel, xlim, ylim, xscale, yscale, legend
+        )
+        self.X, self.Y, self.fmts = None, None, fmts
+
+    def add(self, x, y):
+        # 向图表中添加多个数据点
+        if not hasattr(y, "__len__"):
+            y = [y]
+        n = len(y)
+        if not hasattr(x, "__len__"):
+            x = [x] * n
+        if not self.X:
+            self.X = [[] for _ in range(n)]
+        if not self.Y:
+            self.Y = [[] for _ in range(n)]
+        for i, (a, b) in enumerate(zip(x, y)):
+            if a is not None and b is not None:
+                self.X[i].append(a)
+                self.Y[i].append(b)
+        self.axes[0].cla()
+        for x, y, fmt in zip(self.X, self.Y, self.fmts):
+            self.axes[0].plot(x, y, fmt)
+        self.config_axes()
+        display.display(self.fig)
+        display.clear_output(wait=True)
+
+
+def try_gpu(i=0):
+    if torch.cuda.device_count() >= i + 1:
+        return torch.device(f"cuda:{i}")
+    return torch.device("cpu")
+
+
+def try_all_gpus():
+    devices = []
+    for i in range(torch.cuda.device_count()):
+        devices.append(torch.device(f"cuda:{i}"))
+    if not devices:
+        devices = [torch.device("cpu")]
+    return devices
+
+
+def evaluate_accuracy_gpu(net, data_iter, device=None):
+    if isinstance(net, nn.Module):
+        net.eval()
+        if not device:
+            device = next(iter(net.parameters())).device
+    metric = d2l.Accumulator(2)
+    with torch.no_grad():
+        for X, y in data_iter:
+            if isinstance(X, list):
+                X = [x.to(device) for x in X]
+            else:
+                X = X.to(device)
+            y = y.to(device)
+            metric.add(accuracy(net(X), y), y.numel())
+    return metric[0] / metric[1]
+
+
+def init_weights_xavier(m):
+    if isinstance(m, (nn.Linear, nn.Conv2d)):
+        nn.init.xavier_uniform_(m.weight)
+
+
+def train_gpu(
+    net, train_iter, test_iter, num_epochs, loss, updater, device=None, init=None
+):
+    if init:
+        net.apply(init)
+    print(f"Training on {device}")
+    net.to(device)
+    animator = d2l.Animator(
+        xlabel="epoch",
+        xlim=[1, num_epochs],
+        legend=["train loss", "train acc", "test acc"],
+    )
+    timer, num_batches = d2l.Timer(), len(train_iter)
+    for epoch in range(num_epochs):
+        metric = d2l.Accumulator(3)
+        net.train()
+        for i, (X, y) in enumerate(train_iter):
+            updater.zero_grad()
+            X, y = X.to(device), y.to(device)
+            y_hat = net(X)
+            l = loss(y_hat, y)
+            l.backward()
+            updater.step()
+            with torch.no_grad():
+                metric.add(l.item() * X.shape[0], accuracy(y_hat, y), X.shape[0])
+            train_loss = metric[0] / metric[2]
+            train_acc = metric[1] / metric[2]
+            if (i + 1) % (num_batches // 5) == 0 or i == num_batches - 1:
+                animator.add(
+                    epoch + (i + 1) / num_batches, (train_loss, train_acc, None)
+                )
+        test_acc = evaluate_accuracy_gpu(net, test_iter, device)
+        animator.add(epoch + 1, (None, None, test_acc))
+    print(
+        f"loss {train_loss:.3f}, train acc {train_acc:.3f}, " f"test acc {test_acc:.3f}"
+    )
+    print(
+        f"{metric[2] * num_epochs / timer.sum():.1f} examples/sec " f"on {str(device)}"
+    )
